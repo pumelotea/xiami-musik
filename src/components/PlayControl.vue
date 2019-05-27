3d86d982b98e3a78c3c2a0300b8e0addbbc5c8c0
pumelotea <535553297@qq.com>
2019-05-27 15:24:02 +0800

    播放控制事件释放优化

--- src/components/PlayControl.vue
+++ src/components/PlayControl.vue
@@ -222,6 +222,7 @@
           document.onmousemove = null
           //设置播放进度为拖动的进度
           that.audio.currentTime =  that.audio.duration * that.progress
+          document.onmouseup=null
         }
       }
     },
