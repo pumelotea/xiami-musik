9646c21c4164e6165d487caace5699647d5fda86
pumelotea <535553297@qq.com>
2019-05-29 15:11:08 +0800

    榜单实现完成

---
+++ src/components/Tab2.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="tabs">
+    <span class="tab-item" :class="{actived:currentTab===0}"><a @click="currentTab=0">热歌</a></span>
+    <span class="tab-item" :class="{actived:currentTab===1}"><a @click="currentTab=1">精选</a></span>
+    <span class="tab-item" :class="{actived:currentTab===2}"><a @click="currentTab=2">原创</a></span>
+    <div class="tab-line" :style="'transform: translateX('+58*currentTab+'px)'"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        currentTab:0
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+
+  .tabs {
+    position: relative;
+    padding: 5px;
+  }
+  .tab-item {
+    font-size: 14px;
+    color: #3c3c3c;
+    margin-right: 30px;
+    cursor: pointer;
+  }
+
+  .tabs .actived {
+    color: rgba(255, 50, 0, 1);
+    padding-bottom: 4px;
+    font-weight: bold;
+
+  }
+
+  .tab-line {
+    position: absolute;
+    width: 28px;
+    height: 2px;
+    background: red;
+    bottom: 0;
+    transition: 0.3s;
+  }
+
+  .tab-item:hover {
+    color: rgba(255, 50, 0, 1);
+    padding-top: 10px;
+  }
+</style>
--- src/views/billboard/index.vue
+++ src/views/billboard/index.vue
@@ -1,28 +1,82 @@
 <template>
-  <div class="padder">
-    <div class="official-board">1</div>
-    <div class="board2">
-      <div class="list-2" v-for="x in 2">
-        <div class="list-item" style="padding-right: 20px;">
-          <BillboardItem2/>
+  <div>
+    <div class="official-board">
+      <div class="padder">
+        <div class="title-bar">
+          <div class="board-title" style="float: left">虾米官方榜</div>
+          <div style="float: left;margin-left: 30px">
+            <Tab/>
+          </div>
         </div>
-        <div class="list-item">
-          <BillboardItem2/>
-        </div>
-      </div>
-      <div class="list-2">
-        <div class="list-item" style="padding-right: 20px;">
-          <BillboardItem2/>
-        </div>
-        <div class="list-item">
+        <div class="board2-content">
+          <!--左侧图片-->
+          <div class="music-cover">
+            <img class="music-cover-img" src="texture/music-pic/1.jpg"/>
+          </div>
+          <!--右侧列表-->
+          <div class="music-list">
+            <div class="list-title-bar">
+              <div class="play-all"><i style="color: rgba(255, 50, 0, 1);" class="icon-control-play"></i> 全部播放</div>
+              <div class="all-music">全部 <i class="fa fa-angle-right"></i></div>
+            </div>
+            <div class="list-row-1">
+              <div class="list-item-no">01</div>
+              <div class="list-item-name">灰姑娘</div>
+              <div class="list-item-singer">陈雪凝</div>
+            </div>
+            <div class="list-row-2">
+              <div class="list-item-no">02</div>
+              <div class="list-item-name">灰姑娘</div>
+              <div class="list-item-singer">陈雪凝</div>
+            </div>
+            <div class="list-row-1">
+              <div class="list-item-no">03</div>
+              <div class="list-item-name">灰姑娘</div>
+              <div class="list-item-singer">陈雪凝</div>
+            </div>
+            <div class="list-row-2">
+              <div class="list-item-no">04</div>
+              <div class="list-item-name">灰姑娘</div>
+              <div class="list-item-singer">陈雪凝</div>
+            </div>
+            <div class="list-row-1">
+              <div class="list-item-no">05</div>
+              <div class="list-item-name">灰姑娘</div>
+              <div class="list-item-singer">陈雪凝</div>
+            </div>
+            <div class="list-row-2">
+              <div class="list-item-no">05</div>
+              <div class="list-item-name">灰姑娘</div>
+              <div class="list-item-singer">陈雪凝</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
+    <div class="padder">
+      <div class="board2">
+        <div class="list-2" v-for="x in 2">
+          <div class="list-item" style="padding-right: 20px;">
+            <BillboardItem2/>
+          </div>
+          <div class="list-item">
+            <BillboardItem2/>
+          </div>
+        </div>
+        <div class="list-2">
+          <div class="list-item" style="padding-right: 20px;">
+            <BillboardItem2/>
+          </div>
+          <div class="list-item">
+          </div>
+        </div>
+      </div>
 
-    <div class="board3">
-      <div class="title">全球媒体榜</div>
-      <div class="list-1" v-for="x in 2">
-        <BillboardItem v-for="x in 4"/>
+      <div class="board3">
+        <div class="title">全球媒体榜</div>
+        <div class="list-1" v-for="x in 2">
+          <BillboardItem v-for="x in 4"/>
+        </div>
       </div>
     </div>
   </div>
@@ -31,9 +85,11 @@
 <script>
   import BillboardItem from '@/components/BillboardItem'
   import BillboardItem2 from '@/components/BillboardItem2'
+  import Tab from '@/components/Tab2'
+
   export default {
-    components:{
-      BillboardItem,BillboardItem2
+    components: {
+      BillboardItem, BillboardItem2, Tab
     }
   }
 </script>
@@ -47,13 +103,12 @@
   .title {
     font-size: 24px;
     color: #444444;
-    padding: 5px;
   }
 
   .official-board {
     width: 100%;
     height: 350px;
-    background: #cac8ca;
+    background: #bfbfbf;
   }
 
   .board2 {
@@ -72,15 +127,116 @@
     margin-bottom: 60px;
   }
 
-  .list-2{
+  .list-2 {
     position: relative;
     display: flex;
     margin-bottom: 40px;
   }
 
-  .list-item{
+  .list-item {
     width: 50%;
   }
+
+  .title-bar {
+    height: fit-content;
+    float: left;
+    width: 100%
+  }
+
+  .board-title {
+    font-size: 25px;
+    font-weight: bold;
+    color: #434343;
+  }
+
+  .board2-content {
+    float: left;
+    width: 100%;
+    margin-top: 30px
+  }
+
+  .music-cover {
+    width: 30%;
+    height: 200px;
+    float: left;
+    padding-right: 5%;
+    padding-left: 5%;
+    display: flex;
+  }
+
+  .music-cover-img{
+    width: 90%;
+    margin: auto;
+    -webkit-transform: perspective(400px) rotateY(20deg);
+    border-radius: 2px;
+    box-shadow:  -7px 13px 20px 1px #666666
+  }
+
+  .music-list {
+    width: 60%;
+    height: 200px;
+    float: left;
+  }
+
+  .list-title-bar{
+    float: left;
+    width: 100%;
+    padding: 5px;
+  }
+
+  .list-row-1{
+    width: 100%;
+    float: left;
+    background: #cccccc;
+    padding: 5px;
+    font-size: 12px;
+  }
+
+  .list-row-2{
+    width: 100%;
+    float: left;
+    padding: 5px;
+    font-size: 12px;
+  }
+
+  .play-all{
+    float: left;
+    font-size: 12px;
+    color: #434343;
+    cursor: pointer;
+  }
+
+  .play-all:hover{
+    color: rgba(255, 50, 0, 1);
+  }
+
+
+  .all-music{
+    float: right;
+    font-size: 12px;
+    color: #434343;
+    cursor: pointer;
+  }
+
+  .list-item-no{
+    float: left;
+    width: 40px;
+    color: #3a3a3a;
+    text-align: center;
+  }
+
+  .list-item-name{
+    float: left;
+    font-weight: bold;
+    color: #3a3a3a;
+    width: 40%;
+  }
+
+  .list-item-singer{
+    float: left;
+    color: #3a3a3a;
+  }
+
 
 
 </style>
